1d763eaf0f71240b86bacc8d8efb5dd3d2071a3e
Francisco <franciscosm2000>
2021-05-29 21:34:12 -0600

    Inicio v1.1

---
+++ Sistema_Auditoria/Auditoria.Model/Conexion/ConnectionToSQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Auditoria.Model.Conexion
+{
+    abstract class ConnectionToSQL
+    {
+        //String solo lectura 
+        private readonly string connectionString;
+
+        //Constructor
+        public ConnectionToSQL()
+        {
+            //Cadena servidor 
+            connectionString = @"server=tcp:FRANCISCO,12500; DataBase =PawnSystem; User Id = CasaDeEmpeño; Password=1320022077";
+
+        }
+
+        //Solo accedido de una clase eradada
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
