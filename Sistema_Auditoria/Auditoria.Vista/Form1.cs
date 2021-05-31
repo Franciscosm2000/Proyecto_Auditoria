11f85b4b728f9cdf4af5e4f0af2908d0c3fcf47c
Alexander Flores <58058343+alex160799@users.noreply.github.com>
2021-05-30 21:40:43 -0600

    añadido MDI

--- Sistema_Auditoria/Auditoria.Vista/Form1.cs
+++ Sistema_Auditoria/Auditoria.Vista/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Auditoria.Vista.FrameMDI;
 
 namespace Auditoria.Vista
 {
@@ -16,9 +17,32 @@ namespace Auditoria.Vista
             InitializeComponent();
         }
 
+        private void AddWindow<CustomForm>() where CustomForm : Form, new()
+        {
+            Form window = panel5.Controls.OfType<CustomForm>().FirstOrDefault();
+
+            if (window == null)
+            {
+                window = new CustomForm();
+                window.TopLevel = false;
+                window.Dock = DockStyle.Fill;
+                panel5.Controls.Add(window);
+                panel5.Tag = window;
+                window.Show();
+                window.BringToFront();
+            }
+            else
+            {
+                window.BringToFront();
+            }
+        }
+
+
+
+
         private void button1_Click(object sender, EventArgs e)
         {
-
+            AddWindow<Monitoreo>();
         }
     }
 }
